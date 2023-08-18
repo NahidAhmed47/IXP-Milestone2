5c45d5333a993ab190deca16023c8dfce74d548c
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-08-18 21:44:51 +0600

    result operation added by php

--- calculator/index.php
+++ calculator/index.php
@@ -11,23 +11,40 @@
 <body>
     <div class="container">
         <h1>Calculator</h1>
-        <form accept="" method="post">
+        <form accept="index.php" method="post">
             <label>First Input:</label>
-            <input type="number" name="first_input" required>
+            <input type="number" name="first_input">
             <br>
             <label>Second Input:</label>
-            <input type="number" name="second_input" required>
+            <input type="number" name="second_input" >
             <br>
             <div>
                 <input type="submit" name="add" value="+">
                 <input type="submit" name="subtract" value="-">
-                <input type="submit" name="multiply" value="*">
+                <input type="submit" name="multiply" value="X">
                 <input type="submit" name="divide" value="/">
+                <input type="submit" name="clear" value="C">
             </div>
             <br>
-            <label>Result:</label>
-            <!-- <input type="number" name="result" value="<?php echo $result; ?>" disabled> -->
         </form>
+        <label>Result:</label>
+        <h2>
+            <?php
+            $first_input = $_POST['first_input'];
+            $second_input = $_POST['second_input'];
+            if(isset($_POST['add'])){
+                echo $first_input + $second_input;
+            }elseif(isset($_POST['subtract'])){
+                echo $first_input - $second_input;
+            }elseif(isset($_POST['multiply'])){
+                echo $first_input * $second_input;
+            }elseif(isset($_POST['divide'])){
+                echo $first_input / $second_input;
+            }elseif(isset($_POST['clear'])){
+                echo "";
+            }
+            ?>
+        </h2>
     </div>
 </body>
 
